566cbf41330c3ba1037c7840837e8dde3bf218ab
Olesya Vasyunina <olesyavasyunina@yandex.ru>
2025-05-23 18:53:43 +0700

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace шаблон_приложения
+{
+    public partial class Main : Form
+    {
+        private string _userRole;
+        public Main(string userRole)
+        {
+            InitializeComponent();
+            _userRole = userRole;
+            if (_userRole == "1") 
+            {
+                таблица1ToolStripMenuItem.Visible = true;
+            }
+            else if (_userRole == "2")
+            {
+                таблица1ToolStripMenuItem.Visible = false;
+            }
+        }
+
+        private void таблица1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Очистка
+            foreach (Control c in Controls)
+            {
+                if (c != menuStrip1) Controls.Remove(c);
+            }
+
+            // Создание таблицы
+            DataGridView dgv = new DataGridView();
+            dgv.DataSource = usersTableAdapter.GetData();
+
+            // Центрирование
+            dgv.Location = new Point(
+                (Width - dgv.Width) / 2,
+                menuStrip1.Height + (Height - menuStrip1.Height - dgv.Height) / 2
+            );
+
+            Controls.Add(dgv);
+        }
+    }
+}
